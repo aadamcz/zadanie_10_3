87732f28fd8807b2afb6c66b7150861b80c7ca26
Agnieszka Adamczyk <adamcz.aga@gmail.com>
2017-12-10 10:43:39 +0100

    Create index.html, js/scripts, css/style.css

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(function(){
+	var carouselList = $("#carousel ul");
+	setInterval(function(){
+		carouselList.animate({marginLeft:-400},500,function(){
+			$(this).find("li:last").after($(this).find("li:first"));
+			$(this).css({marginLeft:0});
+		})
+	},3000);
+});
+
+
+
